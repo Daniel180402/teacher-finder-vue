9b41bc7b385f7f791ba92de80ae4c979e55fcab0
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-05-30 09:01:20 +0200

    created main pages and connencted with router

--- src/router.js
+++ src/router.js
@@ -1,16 +1,24 @@
 import {createRouter, createWebHistory} from 'vue-router';
 
+import ChoachDetail from './pages/coaches/ChoachDetail.vue';
+import ChoachesList from './pages/coaches/ChoachesList.vue';
+import ChoachRegistration from './pages/coaches/ChoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path: '/', redirect: '/coaches'},
-        {path: '/coaches', component: null},
-        {path: '/coaches/:id', component: null, children: [
-            {path: 'contact', component: null},
+        {path: '/coaches', component: ChoachesList},
+        {path: '/coaches/:id', component: ChoachDetail, children: [
+            {path: 'contact', component: ContactCoach},
         ]},
-        {path: '/register', component: null},
-        {path: '/requests', component: null},
-        {path: '/:notFound(.*)', component: null},
+        {path: '/register', component: ChoachRegistration},
+        {path: '/requests', component: RequestsReceived},
+        {path: '/:notFound(.*)', component: NotFound},
     ],
 });
 
