e1714bdc631f6a95870f9658cd63ce869c608ad2
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-06-01 22:53:55 +0200

    added Vuex and coach data

---
+++ src/pages/coaches/CoachDetail.vue
@@ -0,0 +1,5 @@
+<template>
+    Teachers Details 
+    <router-view></router-view>
+    <router-link to="/coaches/c1/contact">Contact</router-link>
+</template>
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -3,6 +3,28 @@
         Filter
     </section>
     <section>
-        Teachers
+        <div class="controls">
+            <button>Refresh</button>
+            <router-link to="/register">Register as Teacher</router-link>
+        </div>
+        <ul v-if="hasCoaches">
+            <li v-for="coach in filteredCoaches" :key="coach.id">
+                {{ coach.firstName }}
+            </li>
+        </ul>
+        <h3 v-else>We are sorry but there are no teachers available :(</h3>
     </section>
 </template>
+
+<script>
+export default {
+    computed: {
+        filteredCoaches() {
+            return this.$store.getters['coaches/coaches']; //namespace first --- getters name second
+        },
+        hasCoaches() {
+            return this.$store.getters['coaches/hasCoaches']; //namespace first --- getters name second
+        }
+    }
+}
+</script>
